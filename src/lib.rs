24ad9d54a6f8dd6d85b6ac0c82b836ab3a2f43cd
Jacob Zuo <chopinsky@live.com>
2018-01-20 19:40:50 -0600

    small refactors

--- src/lib.rs
+++ src/lib.rs
@@ -28,38 +28,38 @@ impl HttpServer {
     }
 
     pub fn listen(&self, port: u16) {
-        self.start_with(port);
+        start_with(self, port);
+    }
+}
+
+fn start_with(server: &HttpServer, port: u16) {
+
+    let listener: TcpListener;
+    let server_address = SocketAddr::from(([127, 0, 0, 1], port));
+
+    match TcpListener::bind(server_address) {
+        Ok(result) => {
+            println!("Listening for connections on port {}", port);
+            listener = result;
+        },
+        Err(e) => {
+            println!("Unable to start the http server: {}", e);
+            return;
+        }
     }
 
-    fn start_with(&self, port: u16) {
+    let pool = ThreadPool::new(server.pool_size);
 
-        let listener: TcpListener;
-        let server_address = SocketAddr::from(([127, 0, 0, 1], port));
-
-        match TcpListener::bind(server_address) {
-            Ok(result) => {
-                println!("Listening for connections on port {}", port);
-                listener = result;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(s) => {
+                let router = Route::from(&server.router);
+                pool.execute(move || {
+                    handle_connection(s, router);
+                });
             },
             Err(e) => {
-                println!("Unable to start the http server: {}", e);
-                return;
-            }
-        }
-
-        let pool = ThreadPool::new(self.pool_size);
-
-        for stream in listener.incoming() {
-            match stream {
-                Ok(s) => {
-                    let router = Route::from(&self.router);
-                    pool.execute(move || {
-                        handle_connection(s, router);
-                    });
-                },
-                Err(e) => {
-                    panic!("Server is unable to read from the upcoming stream: {}", e);
-                }
+                panic!("Server is unable to read from the upcoming stream: {}", e);
             }
         }
     }
