9b30826b6d5ab3162d731f643d607c79b19f68a5
Jacob Zuo <chopinsky@live.com>
2018-02-14 21:41:08 -0600

    improve session impl

--- src/session.rs
+++ src/session.rs
@@ -1,62 +1,79 @@
 extern crate rand;
 
 use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::ops::*;
 use std::sync::{Arc, Mutex};
-use std::time::{SystemTime};
-use std::thread;
+use std::time::{Duration, SystemTime};
 use rand::Rng;
 
 lazy_static! {
-    static ref STORE: Arc<Mutex<HashMap<u32, HashMap<String, String>>>> = Arc::new(Mutex::new(HashMap::new()));
-
-    static ref LIFE_MAP: Arc<Mutex<HashMap<u32, SystemTime>>> = Arc::new(Mutex::new(HashMap::new()));
+    static ref STORE: Arc<Mutex<HashMap<u32, Session>>> = Arc::new(Mutex::new(HashMap::new()));
+    static ref DEFAULT_LIFETIME: Arc<Mutex<Duration>> = Arc::new(Mutex::new(Duration::new(172800, 0)));
 }
 
 pub struct Session {
     id: u32,
+    expires_at: SystemTime,
+    auto_renew: bool,
     store: HashMap<String, String>,
 }
 
+impl Session {
+    pub fn clone(&self) -> Self {
+        Session {
+            id: self.id,
+            expires_at: self.expires_at.clone(),
+            auto_renew: self.auto_renew,
+            store: self.store.clone(),
+        }
+    }
+
+    pub fn to_owned(&self) -> Self {
+        Session {
+            id: self.id,
+            expires_at: self.expires_at.to_owned(),
+            auto_renew: self.auto_renew,
+            store: self.store.to_owned(),
+        }
+    }
+}
+
 pub trait SessionExchange {
-    fn new() -> Session;
+    fn new() -> Option<Session>;
     fn from_id(id: u32) -> Option<Session>;
-    fn from_or_new(id: u32) -> Session;
+    fn from_or_new(id: u32) -> Option<Session>;
     fn release(id: u32);
+    fn set_default_session_lifetime(lifetime: Duration);
 }
 
 impl SessionExchange for Session {
-    fn new() -> Self {
-        if let Some(new_id) = new_id() {
-            Session {
-                id: new_id,
-                store: HashMap::new(),
-            }
-        } else {
-            Session {
-                id: 0,
-                store: HashMap::new(),
-            }
-        }
+    fn new() -> Option<Self> {
+        new_session()
     }
 
     fn from_id(id: u32) -> Option<Self> {
         if let Ok(store) = STORE.lock() {
             if let Some(val) = store.get(&id) {
-                Some(Session {
-                   id,
-                   store: val.to_owned(),
-                })
-            } else {
-                None
+                if val.expires_at.cmp(&SystemTime::now()) != Ordering::Less {
+                    //found the session, return now
+                    return Some(val.to_owned());
+
+                } else {
+                    //expired, remove it from the store
+                    release(id);
+                    return None;
+
+                }
             }
-        } else {
-            None
         }
+
+        None
     }
 
-    fn from_or_new(id: u32) -> Self {
+    fn from_or_new(id: u32) -> Option<Self> {
         if let Some(session) = Session::from_id(id) {
-            session
+            Some(session)
         } else {
             Session::new()
         }
@@ -65,13 +82,21 @@ impl SessionExchange for Session {
     fn release(id: u32) {
         release(id);
     }
+
+    fn set_default_session_lifetime(lifetime: Duration) {
+        if let Ok(mut default_lifetime) = DEFAULT_LIFETIME.lock() {
+            *default_lifetime = lifetime;
+        }
+    }
 }
 
 pub trait SessionHandler {
     fn get_id(&self) -> u32;
     fn get_value(&self, key: &str) -> Option<String>;
     fn set_value(&mut self, key: &str, val: &str) -> Option<String>;
-    fn save(&self);
+    fn auto_lifetime_renew(&mut self, auto_renew: bool);
+    fn expires_at(&mut self, expires_at: SystemTime);
+    fn save(&mut self);
 }
 
 impl SessionHandler for Session {
@@ -93,70 +118,87 @@ impl SessionHandler for Session {
         self.store.insert(key.to_owned(), val.to_owned())
     }
 
-    fn save(&self) {
-        save(self.id, self.store.to_owned());
+    fn auto_lifetime_renew(&mut self, auto_renew: bool) {
+        self.auto_renew = auto_renew;
+    }
+
+    fn expires_at(&mut self, expires_time: SystemTime) {
+        if self.auto_renew {
+            self.auto_renew = false;
+        }
+
+        self.expires_at = expires_time;
+    }
+
+    fn save(&mut self) {
+        save(self.id, self);
     }
 }
 
-fn new_id() -> Option<u32> {
+fn new_session() -> Option<Session> {
     if let Ok(mut store) = STORE.lock() {
+        let now = SystemTime::now();
         let mut rng = rand::thread_rng();
         let mut next_id = rng.gen::<u32>();
 
         loop {
             if !store.contains_key(&next_id) { break; }
+
+            if let Ok(elapsed) = now.elapsed() {
+                // 1 sec for get a good guess is already too expansive...
+                if elapsed.as_secs() > 1 { return None; }
+            }
+
+            // now take the next guess
             next_id = rng.gen::<u32>();
         }
 
-        store.insert(next_id, HashMap::new());
-        thread::spawn(move || {
-            update_last_access(next_id, false);
-        });
+        let session = Session {
+            id: next_id,
+            expires_at: get_expires_lifetime(),
+            auto_renew: true,
+            store: HashMap::new(),
+        };
 
-        Some(next_id)
+        store.insert(next_id, session.to_owned());
+        Some(session)
+
     } else {
-        return None;
+        None
+
     }
 }
 
-fn save(id: u32, content: HashMap<String, String>) -> bool {
+fn save(id: u32, session: &mut Session) -> bool {
     if let Ok(mut store) = STORE.lock() {
-        let old_store = store.insert(id, content);
-        drop(old_store);
+        if session.auto_renew {
+            session.expires_at = get_expires_lifetime();
+        }
 
-        thread::spawn(move || {
-            update_last_access(id, false);
-        });
+        let old_store = store.insert(id, session.to_owned());
+        drop(old_store);
 
     } else {
         return false;
     }
 
     true
+}
+
+fn get_expires_lifetime() -> SystemTime {
+    if let Ok(default_lifetime) = DEFAULT_LIFETIME.lock() {
+        SystemTime::now().add(*default_lifetime)
+    } else {
+        SystemTime::now().add(Duration::new(172800, 0))
+    }
 }
 
 fn release(id: u32) -> bool {
     if let Ok(mut store) = STORE.lock() {
         store.remove(&id);
-
-        thread::spawn(move || {
-            update_last_access(id, true);
-        });
-
     } else {
         return false;
     }
 
     true
-}
-
-fn update_last_access(id: u32, to_remove: bool) {
-    if let Ok(mut map) = LIFE_MAP.lock() {
-        if !to_remove {
-            let now = SystemTime::now();
-            map.insert(id, now);
-        } else {
-            map.remove(&id);
-        }
-    }
 }
