c077d7e5b0a8aac4a7f0b194e7d2d6f76600eb0e
Jacob Zuo <chopinsky@live.com>
2019-08-11 18:02:43 -0500

    update the sync.pool

--- src/core/syncstore.rs
+++ src/core/syncstore.rs
@@ -1,16 +1,17 @@
-#![allow(dead_code)]
+#![allow(unused)]
 
 use std::io::ErrorKind;
 use std::mem::MaybeUninit;
 use std::ops::{Deref, DerefMut};
-use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::ptr;
+use std::sync::atomic::{AtomicBool, AtomicU16, AtomicUsize, Ordering};
 
 use crate::support::common::cpu_relax;
 
-const POOL_SIZE: usize = 8;
-const SLOT_CAP: usize = 16;
-const EXPANSION_CAP: usize = 512;
-const EXPANSION_THRESHOLD: usize = 8;
+const POOL_SIZE: usize = 16;
+const SLOT_CAP: usize = 8;
+const GET_MASK: u16 = 0b1010_1010_1010_1010;
+const PUT_MASK: u16 = 0b1111_1111_1111_1111;
 
 struct Slot<T> {
     /// the actual data store
@@ -45,7 +46,7 @@ impl<T: Default> Slot<T> {
 
     fn try_lock(&self, is_get: bool) -> bool {
         // be more patient if we're to return a value
-        let mut count = if is_get { 4 } else { 6 };
+        let mut count = if is_get { 2 } else { 4 };
 
         // check the lock and wait if not available
         while self
@@ -53,13 +54,13 @@ impl<T: Default> Slot<T> {
             .compare_exchange(false, true, Ordering::SeqCst, Ordering::Relaxed)
             .is_err()
         {
-            cpu_relax(2 * count);
-            count -= 1;
-
             // "timeout" -- tried 4 times and still can't get the try_lock, rare case but fine, move on.
+            count -= 1;
             if count == 0 {
                 return false;
             }
+
+            cpu_relax(2 * count);
         }
 
         if (is_get && self.len == 0) || (!is_get && self.len == SLOT_CAP) {
@@ -79,7 +80,7 @@ impl<T: Default> Slot<T> {
 
     /// The function is safe because it's used internally, and each time it's guaranteed a try_lock has
     /// been acquired previously
-    fn get_one(&mut self) -> T {
+    fn checkout(&mut self) -> T {
         // need to loop over the slots to make sure we're getting the valid value, starting from
         for i in (0..self.len).rev() {
             if let Some(val) = self.slot[i].take() {
@@ -96,13 +97,13 @@ impl<T: Default> Slot<T> {
 
     /// The function is safe because it's used internally, and each time it's guaranteed a try_lock has
     /// been acquired previously
-    fn put_one(&mut self, val: T) {
+    fn release(&mut self, val: T) {
         // need to loop over the slots to make sure we're getting the valid value
         for i in self.len..SLOT_CAP {
             if self.slot[i].is_none() {
                 // update internal states
                 self.slot[i].replace(val);
-                self.len = i;
+                self.len = i + 1;
 
                 // done
                 return;
@@ -111,6 +112,173 @@ impl<T: Default> Slot<T> {
 
         // if all slots are full, no need to fallback, the `val` will be dropped here
         drop(val);
+    }
+}
+
+pub(crate) struct Bucket<T> {
+    /// the actual data store
+    slot: [Option<T>; SLOT_CAP],
+
+    /// the current ready-to-use slot index, always offset by 1 to the actual index
+    len: AtomicUsize,
+
+    bitmap: AtomicU16,
+}
+
+impl<T: Default> Bucket<T> {
+    pub(crate) fn new(fill: bool) -> Self {
+        // create the placeholder
+        let mut slice: [Option<T>; SLOT_CAP] = unsafe { MaybeUninit::zeroed().assume_init() };
+        let mut bitmap: u16 = 0;
+
+        // fill the slots and update the bitmap
+        if fill {
+            for (i, item) in slice.iter_mut().enumerate() {
+                item.replace(Default::default());
+                bitmap |= 1 << (2 * i as u16);
+            }
+        }
+
+        // done
+        Bucket {
+            slot: slice,
+            len: AtomicUsize::new(SLOT_CAP),
+            bitmap: AtomicU16::new(bitmap),
+        }
+    }
+
+    pub(crate) fn access(&self, get: bool) -> Result<usize, ()> {
+        // pre-checkout, make sure the len is in post-action state so it can reject future attempts
+        // if it's unlikely to succeed in this slot.
+        let curr_len = if get {
+            self.len.fetch_sub(1, Ordering::AcqRel)
+        } else {
+            self.len.fetch_add(1, Ordering::AcqRel)
+        };
+
+        // oops, last op blew off the roof, back off mate. Note that (0 - 1 == MAX_USIZE) for stack
+        // overflow, still way off the roof and a proof of not doing well.
+        if curr_len > SLOT_CAP || (get && curr_len == 0) {
+            return self.access_failure(get);
+        }
+
+        let mut trials: usize = 2;
+        while trials > 0 {
+            // init try
+            let (pos, mask) = match self.enter(get) {
+                Ok(pos) => (pos, 0b10 << (2 * pos)),
+                Err(()) => {
+                    return self.access_failure(get);
+                },
+            };
+
+            // main loop to try to update the bitmap
+            let old = self.bitmap.fetch_or(mask, Ordering::AcqRel);
+
+            // if the lock bit we replaced was not yet marked at the atomic op, we're good
+            if old & mask == 0 {
+                return Ok(pos as usize)
+            }
+
+            // otherwise, try again after some wait
+            cpu_relax(4 * trials);
+            trials -= 1;
+        }
+
+        self.access_failure(get)
+    }
+
+    pub(crate) fn leave(&self, pos: u16) {
+        let lock_bit = 0b10 << (2 * pos);
+
+        loop {
+            let old = self.bitmap.fetch_xor(0b11 << (2 * pos), Ordering::SeqCst);
+            if old & lock_bit == lock_bit {
+                return;
+            }
+        }
+    }
+
+    /// The function is safe because it's used internally, and each time it's guaranteed a access has
+    /// been acquired previously
+    pub(crate) fn checkout(&mut self, pos: usize) -> Result<T, ()> {
+        // check if it's a valid position to swap out the value
+        if self.slot[pos].is_none() {
+            return Err(());
+        }
+
+        // return the value
+        Ok(self.swap_out(pos))
+    }
+
+    /// The function is safe because it's used internally, and each time it's guaranteed a access has
+    /// been acquired previously
+    pub(crate) fn release(&mut self, pos: usize, val: T) {
+        // need to loop over the slots to make sure we're getting the valid value
+        if pos >= SLOT_CAP {
+            return;
+        }
+
+        if self.slot[pos].is_some() {
+            // if all slots are full, no need to fallback, the `val` will be dropped here
+            drop(val);
+            return;
+        }
+
+        // move the value in
+        self.swap_in(pos, val);
+    }
+
+    #[inline]
+    fn access_failure(&self, get: bool) -> Result<usize, ()> {
+        if get {
+            self.len.fetch_add(1, Ordering::AcqRel);
+        } else {
+            self.len.fetch_sub(1, Ordering::AcqRel);
+        }
+
+        Err(())
+    }
+
+    fn swap_in(&mut self, index: usize, content: T) {
+        let src = &mut self.slot[index] as *mut Option<T>;
+        unsafe {
+            src.write(Some(content));
+        }
+    }
+
+    fn swap_out(&mut self, index: usize) -> T {
+        let src = &mut self.slot[index] as *mut Option<T>;
+
+        unsafe {
+            // save off the old values
+            let val = ptr::read(src).unwrap_or_default();
+
+            // swap values
+            src.write(None);
+
+            val
+        }
+    }
+
+    /// Assuming we have 8 elements per slot, otherwise must update the assumption.
+    fn enter(&self, get: bool) -> Result<u16, ()> {
+        let src = self.bitmap.load(Ordering::Acquire);
+
+        let mut pos = 0;
+        let mut base = if get { src ^ GET_MASK } else { src ^ PUT_MASK };
+
+        while base > 0 {
+            if base & 0b11 == 0b11 {
+                // update the state and the position
+                return Ok(pos);
+            }
+
+            pos += 1;
+            base >>= 2;
+        }
+
+        Err(())
     }
 }
 
@@ -142,7 +310,7 @@ impl<'a> Drop for VisitorGuard<'a> {
 
 pub(crate) struct SyncPool<T> {
     /// The slots storage
-    slots: Vec<Slot<T>>,
+    slots: Vec<Bucket<T>>,
 
     /// the next channel to try
     curr: AtomicUsize,
@@ -155,9 +323,6 @@ pub(crate) struct SyncPool<T> {
     ///   true  -> write barrier has been raised
     ///   false -> no write barrier
     visitor_counter: (AtomicUsize, AtomicBool),
-
-    /// the number of times we failed to find an in-store struct to offer
-    fault_count: AtomicUsize,
 }
 
 impl<T: Default> SyncPool<T> {
@@ -174,90 +339,110 @@ impl<T: Default> SyncPool<T> {
         Self::make_pool(pool_size)
     }
 
-    pub(crate) fn get(&mut self) -> T {
+    pub fn get(&mut self) -> T {
+        // update user count
+        let _guard = VisitorGuard::register(&self.visitor_counter);
+
+        // start from where we're left
+        let cap = self.slots.len();
+        let origin: usize = self.curr.fetch_add(1, Ordering::AcqRel) % cap;
+
+        let mut pos = origin;
+        let mut trials = cap;
+
+        loop {
+            // check this slot
+            let slot = &mut self.slots[pos];
+
+            // try the access or move on
+            if let Ok(i) = slot.access(true) {
+                // try to checkout one slot
+                let checkout = slot.checkout(i);
+                slot.leave(i as u16);
+
+            /*
+            if slot.access(true) {
+                // try to checkout one slot
+                let checkout = slot.checkout();
+                slot.leave();
+            */
+
+                if let Ok(val) = checkout {
+                    // now we're locked, get the val and update internal states
+                    self.curr.store(pos, Ordering::Release);
+
+                    // done
+                    return val;
+                }
+
+                // failed to checkout, break and let the remainder logic to handle the rest
+                break;
+            }
+
+            // update to the next position now.
+            pos = self.curr.fetch_add(1, Ordering::AcqRel) % cap;
+            trials -= 1;
+
+            // we've finished 1 loop but not finding a value to extract, quit
+            if trials == 0 || pos == origin {
+                break;
+            }
+        }
+
+        // make sure our guard has been returned if we want the correct visitor count
+        drop(_guard);
+
+        Default::default()
+    }
+
+    pub fn put(&mut self, val: T) {
         // update user count
         let _guard = VisitorGuard::register(&self.visitor_counter);
 
         // start from where we're left
         let cap = self.slots.len();
         let origin: usize = self.curr.load(Ordering::Acquire) % cap;
+
         let mut pos = origin;
+        let mut trials = cap / 2;
 
         loop {
             // check this slot
-            let slot: &mut Slot<T> = &mut self.slots[pos];
-            let next = if pos == cap - 1 { 0 } else { pos + 1 };
+            let bucket = &mut self.slots[pos];
 
-            // try the try_lock or move on
-            if !slot.try_lock(true) {
-                pos = next;
+            // try the access or move on
+            if let Ok(i) = bucket.access(false) {
+                // now we're locked, get the val and update internal states
+                self.curr.store(pos, Ordering::Release);
 
-                // we've finished 1 loop but not finding a value to extract, quit
-                if pos == origin {
-                    break;
-                }
+                // put the value back and reset
+                bucket.release(i, val);
+                bucket.leave(i as u16);
 
-                continue;
+                return;
             }
 
-            // now we're locked, get the val and update internal states
-            self.curr.store(next, Ordering::Release);
-            let val = slot.get_one();
-            slot.unlock();
+            /*
+            if slot.access(false) {
+                // now we're locked, get the val and update internal states
+                self.curr.store(pos, Ordering::Release);
 
-            // done
-            return val;
-        }
+                // put the value back into the slot
+                slot.release(val, self.reset_handle.load(Ordering::Acquire));
+                slot.leave();
 
-        // make sure our guard has been returned if we want the correct visitor count
-        drop(_guard);
-
-        if self.fault_count.fetch_add(1, Ordering::AcqRel) > EXPANSION_THRESHOLD
-            && cap < EXPANSION_CAP
-            && self.expand(1, false)
-        {
-            self.fault_count.store(0, Ordering::Release);
-        }
-
-        Default::default()
-    }
-
-    pub(crate) fn put(&mut self, val: T) {
-        // update user count
-        let _guard = VisitorGuard::register(&self.visitor_counter);
-
-        // start from where we're left
-        let cap = self.slots.len();
-        let curr: usize = self.curr.load(Ordering::Acquire) % cap;
-
-        // origin is 1 `Slots` off from the next "get" position
-        let origin = if curr > 0 { curr - 1 } else { 0 };
-
-        let mut pos = origin;
-
-        loop {
-            // check this slot
-            let slot: &mut Slot<T> = &mut self.slots[pos];
-            let next = if pos == 0 { cap - 1 } else { pos - 1 };
-
-            // try the try_lock or move on
-            if !slot.try_lock(false) {
-                pos = next;
-
-                // we've finished 1 loop but not finding a value to extract, quit
-                if pos == origin {
-                    break;
-                }
-
-                continue;
+                return;
             }
+            */
 
-            // now we're locked, get the val and update internal states
-            self.curr.store(pos, Ordering::Release);
-            slot.put_one(val);
-            slot.unlock();
+            // update states
+            pos = self.curr.fetch_sub(1, Ordering::AcqRel) % cap;
+            trials -= 1;
 
-            return;
+            // we've finished 1 loop but not finding a value to extract, quit
+            if trials == 0 || pos == origin {
+                break;
+            }
         }
     }
 
@@ -267,7 +452,7 @@ impl<T: Default> SyncPool<T> {
         if self
             .visitor_counter
             .1
-            .compare_exchange_weak(false, true, Ordering::Acquire, Ordering::Relaxed)
+            .compare_exchange_weak(false, true, Ordering::Acquire, Ordering::Acquire)
             .is_err()
         {
             return false;
@@ -279,7 +464,7 @@ impl<T: Default> SyncPool<T> {
             match self
                 .visitor_counter
                 .0
-                .compare_exchange(1, 0, Ordering::SeqCst, Ordering::SeqCst)
+                .compare_exchange(1, 0, Ordering::SeqCst, Ordering::Relaxed)
             {
                 Ok(_) => break true,
                 Err(old) => {
@@ -296,13 +481,13 @@ impl<T: Default> SyncPool<T> {
         if safe {
             // update the slots by pushing `additional` slots
             (0..additional).for_each(|_| {
-                self.slots.push(Slot::new(true));
+                self.slots.push(Bucket::new(true));
             });
-
-            // update the internal states
-            self.visitor_counter.0.store(1, Ordering::SeqCst);
-            self.visitor_counter.1.store(false, Ordering::Release);
         }
+
+        // update the internal states
+        self.visitor_counter.0.store(1, Ordering::SeqCst);
+        self.visitor_counter.1.store(false, Ordering::Release);
 
         safe
     }
@@ -312,14 +497,13 @@ impl<T: Default> SyncPool<T> {
 
         (0..size).for_each(|_| {
             // add the slice back to the vec container
-            s.push(Slot::new(true));
+            s.push(Bucket::new(true));
         });
 
         SyncPool {
             slots: s,
             curr: AtomicUsize::new(0),
             visitor_counter: (AtomicUsize::new(1), AtomicBool::new(false)),
-            fault_count: AtomicUsize::new(0),
         }
     }
 }
